6a012ea99ae3b44fc55142623b239fe90430f03f
Maria Lind <mal@mjolner.dk>
2022-05-10 11:47:50 +0200

    forms

--- lib/details_page.dart
+++ lib/details_page.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:profile_maker/shared/bottom_navigation.dart';
 
@@ -9,6 +11,7 @@ class DetailsPage extends StatefulWidget {
 }
 
 class _DetailsPageState extends State<DetailsPage> {
+  final _detailsFormKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -18,16 +21,72 @@ class _DetailsPageState extends State<DetailsPage> {
         title: const Text('Profile Maker: Details'),
       ),
       body: Center(
-
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
+            Padding(padding: const EdgeInsets.symmetric(horizontal: 32.0, vertical: 16.0), child: (
+                Form(key: _detailsFormKey,
+                    child: Column(
+                      children: <Widget>[
+                        TextFormField(
+                          decoration: const InputDecoration(
+                            icon: Icon(Icons.person),
+                            hintText: 'What do people call you?',
+                            labelText: 'Name *',
+                          ),
+                          validator: (value) {
+                            if(value == null || value.isEmpty) {
+                              return 'Enter your name';
+                            }
+                            return null;
+                          },
+                        ),
+                        TextFormField(
+                          decoration: const InputDecoration(
+                            icon: Icon(Icons.email),
+                            hintText: 'How to contact you?',
+                            labelText: 'Email *',
+                          ),
+                          keyboardType: TextInputType.emailAddress,
+                          validator: (value) {
+                            if(value == null || value.isEmpty) {
+                              return 'Enter your email';
+                            }
+                            if(!RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(value)) {
+                              return 'This is not a valid email';
+                            }
+                            return null;
+                          },
+
+                        ),
+                        TextFormField(
+                          decoration: const InputDecoration(
+                            icon: Icon(Icons.phone_android),
+                            hintText: 'How to contact you?',
+                            labelText: 'Mobile *',
+                          ),
+                          keyboardType: TextInputType.number,
+                          validator: (value) {
+                            if(value == null || value.isEmpty) {
+                              return 'Enter your mobile';
+                            }
+                            return null;
+                          },
+                        ),
+                      ],
+                    )
+                )
+              )
+            ),
             ElevatedButton(
                 onPressed: () {
-                  Navigator.pushNamed(
-                    context,
-                    '/step3',
-                  );
+                  if(_detailsFormKey.currentState!.validate()){
+                    Navigator.pushNamed(
+                      context,
+                      '/step3',
+                    );
+                  }
+
                 },
                 child: const Text('Preferences'))
           ],
--- lib/preferences_page.dart
+++ lib/preferences_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:profile_maker/shared/bottom_navigation.dart';
 
+
+enum FavoriteFoods { pizza, steak, fries, risotto, burger }
+
 class PreferencesPage extends StatefulWidget {
   const PreferencesPage({Key? key}) : super(key: key);
 
@@ -9,6 +12,73 @@ class PreferencesPage extends StatefulWidget {
 }
 
 class _PreferencesPageState extends State<PreferencesPage> {
+  FavoriteFoods? _favoriteFood = FavoriteFoods.pizza;
+
+  returnOptions() {
+    return <Widget>[
+      ListTile(
+        title: const Text('Pizza'),
+        leading: Radio<FavoriteFoods>(
+          value: FavoriteFoods.pizza,
+          groupValue: _favoriteFood,
+          onChanged: (FavoriteFoods? value) {
+            setState(() {
+              _favoriteFood = value;
+            });
+          },
+        ),
+      ),
+      ListTile(
+        title: const Text('Steak'),
+        leading: Radio<FavoriteFoods>(
+          value: FavoriteFoods.steak,
+          groupValue: _favoriteFood,
+          onChanged: (FavoriteFoods? value) {
+            setState(() {
+              _favoriteFood = value;
+            });
+          },
+        ),
+      ),
+      ListTile(
+        title: const Text('Fries'),
+        leading: Radio<FavoriteFoods>(
+          value: FavoriteFoods.fries,
+          groupValue: _favoriteFood,
+          onChanged: (FavoriteFoods? value) {
+            setState(() {
+              _favoriteFood = value;
+            });
+          },
+        ),
+      ),
+      ListTile(
+        title: const Text('Risotto'),
+        leading: Radio<FavoriteFoods>(
+          value: FavoriteFoods.risotto,
+          groupValue: _favoriteFood,
+          onChanged: (FavoriteFoods? value) {
+            setState(() {
+              _favoriteFood = value;
+            });
+          },
+        ),
+      ),
+      ListTile(
+        title: const Text('Burger'),
+        leading: Radio<FavoriteFoods>(
+          value: FavoriteFoods.burger,
+          groupValue: _favoriteFood,
+          onChanged: (FavoriteFoods? value) {
+            setState(() {
+              _favoriteFood = value;
+            });
+          },
+        ),
+      )
+    ];
+  }
+
   @override
   Widget build(BuildContext context) {
 
@@ -19,8 +89,15 @@ class _PreferencesPageState extends State<PreferencesPage> {
       body: Center(
 
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
+            Padding(padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0), child: (
+                Column(
+                      children: returnOptions()
+                    )
+                )
+            ),
+
             ElevatedButton(
                 onPressed: () {
                   Navigator.popUntil(context, (route) => route.isFirst);
